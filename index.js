8b8b3fbeac666d625c15519e31b224432ad8da5a
Brian Sandoval <39751822+iambrianshin@users.noreply.github.com>
2019-03-29 12:14:12 -0400

    Adding items to the shopping list

--- index.js
+++ index.js
@@ -1,57 +1,79 @@
-'use strict';
+"use strict";
 
-function renderShoppingList(){
+const STORE = [
+  { name: "apples", checked: false },
+  { name: "oranges", checked: false },
+  { name: "milk", checked: true },
+  { name: "bread", checked: false }
+];
+
+function generateItemList(item, itemIndex) {
+  const checkedClass = item.checked ? "shopping-item__checked" : "";
+
+  return `
+      <li class="js-item-index-element" data-item-index="${itemIndex}">
+        <span class="shopping-item js-shopping-item ${checkedClass}">${
+    item.name
+  }</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+     </li>
+    `;
+}
+
+function generateShoppingListItem(shoppingList) {
+  const items = shoppingList.map(item => generateItemList(item));
+
+  return items.join("");
+}
+
+function renderShoppingList() {
   // grab STORE
   // generate html
   // render
-  console.log('renderShoppingList accessed');
+  console.log("renderShoppingList accessed");
+  const shoppingListsItems = generateShoppingListItem(STORE);
+
+  $(".js-shopping-list").html(shoppingListsItems);
 }
 
-function handleNewItemSubmit(){
+function handleNewItemSubmit() {
   // get item to be submitted
   // access the STORE
   // add item to the STORE
-  console.log('handleNewItemSubmit accessed');
+  console.log("handleNewItemSubmit accessed");
+  $("#js-shopping-list-form").submit(function(event) {
+    event.preventDefault();
+  });
 }
 
-function handleItemCheckClicked(){
+function handleItemCheckClicked() {
   // get item to be checked
   // access the STORE
   // update the STORE
-  console.log('handleItemCheckClicked accessed');
+  console.log("handleItemCheckClicked accessed");
 }
 
-function handleDeleteItemClicked(){
+function handleDeleteItemClicked() {
   // get item to be deleted
   // access the STORE
   // remove item from the STORE
-  console.log('handleDeleteItemClicked accessed');
+  console.log("handleDeleteItemClicked accessed");
 }
 
-function handleShoppingList(action, item){
+function handleShoppingList() {
   renderShoppingList();
-  // get action to be taken (render,new item, etc.)
-  // invoke the correct function (possibly switch statement?)
-  switch(action) {
-  case 'new':
-    handleNewItemSubmit(item);
-    break;
-  case 'check':
-    handleItemCheckClicked(item);
-    break;
-  case 'delete':
-    handleDeleteItemClicked(item);
-    break;
-  default:
-    // do nothing?
-  }
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
 
-  renderShoppingList();
-  console.log('handleShoppingList accessed');
+  console.log("handleShoppingList accessed");
 }
 
-function main() {
-  handleShoppingList();
-}
-
-$(main);
+$(handleShoppingList);
